e579f6aee3dfeba80c3cf4534bbabfc24ca66b9d
stingbreed <ozoemenezubby@gmail.com>
2023-12-31 18:44:53 +0100

    Server setup

---
+++ server/models/news.js
@@ -0,0 +1,16 @@
+const newsSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+  
+  const News = mongoose.model('News', newsSchema);
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
 
 const PORT = process.env.PORT || 4000;
-const MONGODB_URI = 'YOUR_MONGODB_URI';
+const MONGODB_URI = 'mongodb+srv://ozoemenezubby:gwZGQnhsODgf9uCO@floweb-database-dev.tbt7py1.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
@@ -17,7 +17,33 @@ mongoose.connect(MONGODB_URI, {
 nextApp.prepare().then(() => {
   const app = express();
 
+  // gwZGQnhsODgf9uCO
+
+  // mongodb+srv://ozoemenezubby:gwZGQnhsODgf9uCO@floweb-database-dev.tbt7py1.mongodb.net/?retryWrites=true&w=majority
+
   // Add your API routes here (e.g., app.get, app.post)
+
+  app.post('/api/news', async (req, res) => {
+    try {
+      const { title, content } = req.body;
+      const newNews = new News({ title, content });
+      const savedNews = await newNews.save();
+      res.status(201).json(savedNews);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+  app.get('/api/news', async (req, res) => {
+    try {
+      const allNews = await News.find();
+      res.status(200).json(allNews);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 
   app.all('*', (req, res) => {
     return handle(req, res);
